d0fa8d0c04cd6b9d6d022f302006ccec4ccfb0d0
padsdev <fpadilha.augusto@gmail.com>
2026-06-01 19:12:39 -0300

    inicialização do projeto com setup do Gradle, com algumas dependencias

---
+++ src/main/kotlin/com/devpads/unimed_test/UnimedTestApplication.kt
@@ -0,0 +1,11 @@
+package com.devpads.unimed_test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UnimedTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<UnimedTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/devpads/unimed_test/UnimedTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devpads.unimed_test
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UnimedTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
